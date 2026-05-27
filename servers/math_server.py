422fde834b0039a76934d7024dc4564ff03de840
Abhisehkh Padmanabhan <abhishekh.padmanabhan@veeva.com>
2026-05-27 18:18:51 -0400

    MCP server logging

--- servers/math_server.py
+++ servers/math_server.py
@@ -1,18 +1,22 @@
 # math_server.py
+import logging
 from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP("Math")
 
+# Configure a logger
+logger = logging.getLogger("math_mcp_server")
+
 @mcp.tool()
 def add(a: int, b: int) -> int:
     """Add two numbers"""
-    print(f"a: {a}")
-    print(f"b: {b}")
+    logger.info(f"Running add tool with a={a}, b={b}")
     return a + b
 
 @mcp.tool()
 def multiply(a: int, b: int) -> int:
     """Multiply two numbers"""
+    logger.info(f"Running multiply tool with a={a}, b={b}")
     return a * b
 
 if __name__ == "__main__":
